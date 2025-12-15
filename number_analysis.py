8e6621d5b254338e45351a80960a7169deab5473
Hakan BAL <hakanbal045@gmail.com>
2025-12-15 15:26:26 +0300

    Create number_analysis.py

---
+++ number_analysis.py
@@ -0,0 +1,9 @@
+numbers = [10, 25, 30, 45, 60]
+
+average = sum(numbers) / len(numbers)
+max_number = max(numbers)
+min_number = min(numbers)
+
+print("Ortalama:", average)
+print("En büyük:", max_number)
+print("En küçük:", min_number)
